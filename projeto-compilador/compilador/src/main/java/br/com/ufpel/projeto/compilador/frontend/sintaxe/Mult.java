af05f1a007481181ea465d3b605a1e17efe71cf3
yuji <yujisakuma224@gmail.com>
2024-08-10 16:51:46 -0300

    trabalho 1

---
+++ projeto-compilador/compilador/src/main/java/br/com/ufpel/projeto/compilador/frontend/sintaxe/Mult.java
@@ -0,0 +1,7 @@
+package br.com.ufpel.projeto.compilador.frontend.sintaxe;
+
+public class Mult extends Operador {
+    Mult(Exp arg1, Exp arg2) {
+        super(arg1, arg2);
+    }
+}
---
+++ projeto-compilador/compilador/src/main/java/br/com/ufpel/projeto/compilador/frontend/sintaxe/Sub.java
@@ -0,0 +1,7 @@
+package br.com.ufpel.projeto.compilador.frontend.sintaxe;
+
+public class Sub extends Operador {
+    Sub(Exp arg1, Exp arg2) {
+        super(arg1, arg2);
+    }
+}
